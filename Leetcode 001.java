4e75617d7efe083ee2d6964033df5a1cce853756
yuweiw823 <yuweiw823@gmail.com>
2015-10-10 16:56:01 -0700

    leetcode 001

---
+++ Leetcode 001.java
@@ -0,0 +1,68 @@
+//Leetcode 001
+
+import java.io.*;
+import java.util.*;
+
+//Solution 1 - Hashmap
+class Solution {
+    public static void main(String[] args) {
+        int[] input = {2,3,7};
+        int target = 5;
+        int [] result = twoSum(input, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+    
+    public static int[] twoSum(int[] nums, int target) {
+        int [] res = {0, 0};
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                res[0] = map.get((target - nums[i])) + 1;
+                res[1] = i + 1;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
+
+//Solution 2 - BinarySearch - sorted array
+class Solution {
+    public static void main(String[] args) {
+        int[] input = {2, 7, 11, 15};
+        int target = 26;
+        int [] result = twoSum(input, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+    
+    public static int[] twoSum(int[] nums, int target) {
+        int [] res = {0, 0};
+
+        for(int i=0; i<nums.length; i++){
+            System.out.println(i);
+            int find = BiSearch(nums, i, nums.length-1, target - nums[i]) + 1;
+            if(find > 0){
+                res[1] = find;
+                res[0] = i + 1;
+            }
+        }
+        return res;
+    }
+    
+    public static int BiSearch(int[] A, int start, int end, int key){
+        int L = start;
+        int R = end;
+        
+        while(L<R){
+            int M = L + (R-L)/2;
+            if(A[M] < key) {
+                L = M + 1;
+            } else {
+                R = M;
+            }
+        }
+        return (L == R && A[L] == key) ? L : -1;
+    }
+}
