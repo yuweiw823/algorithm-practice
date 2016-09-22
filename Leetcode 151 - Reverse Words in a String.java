7314726168fe5e605b2ca2c408af22351cce0bbd
Yuwei Wang (Insight Global) <v-yuww@microsoft.com>
2016-09-21 17:00:07 -0700

    9/21

--- Leetcode 151 - Reverse Words in a String.java
+++ Leetcode 151 - Reverse Words in a String.java
@@ -23,3 +23,47 @@ public class Solution {
         return sb.toString();
     }
 }
+
+" " -> ""
+//简单版本：直接用.split()，只适用于仅单词间存在空格，且空格仅有一个的情况
+public class Solution {
+    public String reverseWords(String s) {
+        if(s == null || s.length() == 0) {
+            return "";
+        }
+        
+        String[] words = s.split(" ");
+        StringBuffer sb = new StringBuffer();
+        
+        for(int i = words.length - 1; i >=0; i--) {
+                sb.append(words[i]);
+                sb.append(" ");
+        }
+        
+        return sb.toString().trim();
+    }
+}
+
+"  " -> ""
+"a   b  " -> "b a"
+//以上这种多空格的情况，不能直接用
+public class Solution {
+    public String reverseWords(String s) {
+        if(s == null || s.length() == 0) {
+            return "";
+        }
+        StringBuffer result = new StringBuffer();
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) == ' ') {
+                continue;
+            };
+            
+            int end = i;
+            while(i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+            result.append(s.substring(i + 1, end + 1)).append(" ");
+        }
+        return result.toString().trim();
+    }
+}
