d4c2560b0deed732b153af2e1acde9154188b17d
Yuwei Wang (Insight Global) <v-yuww@microsoft.com>
2016-09-22 16:36:36 -0700

    9/22 update

--- Leetcode 054 - Spiral Matrix - 从边上一层层取.java
+++ Leetcode 054 - Spiral Matrix - 从边上一层层取.java
@@ -12,43 +12,46 @@ public class Solution {
 
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<Integer>();
-        if(matrix.length == 0 || matrix[0].length == 0 || matrix == null)
-            return res;
-        
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int min_di = Math.min(rows, cols);
-        int borderMax = min_di/2;
-        
-        //b is border
-        for(int b=0; b<borderMax; b++){
-            for(int i=b, j=b; j<cols-1-b; j++){
-                res.add(matrix[i][j]);
-            }
-            for(int i=b, j=cols-1-b; i<rows-1-b; i++){
-                res.add(matrix[i][j]);
-            }
-            for(int i=rows-1-b, j=cols-1-b; j>b ; j--){
-                res.add(matrix[i][j]);
-            }
-            for(int i=rows-1-b, j=b; i>b; i--){
-                res.add(matrix[i][j]);
-            }
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(matrix == null || matrix.length == 0) {
+          return result;
         }
         
-        if(min_di % 2 == 1){
-            if(cols<rows){//竖着长
-                for(int i=borderMax, j=borderMax; i<rows-borderMax; i++){
-                    res.add(matrix[i][j]);
-                }
-            }else{
-                for(int i=borderMax, j=borderMax; j<cols-borderMax; j++){
-                    res.add(matrix[i][j]);
-                }
-            } 
-        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int minBorder = Math.min(m, n);
+        int depth = minBorder / 2;
         
-        return res;
+
+        for(int d = 0; d < depth; d++) {
+            for(int i = d, j = d; j < n - 1 - d; j++) {
+                result.add(matrix[i][j]);
+            }
+            
+            for(int i = d, j = n - 1 - d; i < m - 1 - d; i++) {
+                result.add(matrix[i][j]);
+            }
+            
+            for(int i = m - 1 - d, j = n - 1 - d; j > d; j--) {
+                result.add(matrix[i][j]);
+            }
+            
+            for(int i = m - 1 - d, j = d; i > d; i--) {
+                result.add(matrix[i][j]);
+            }
+        }  
+        
+        if(minBorder % 2 == 1) {
+            if(m <= n) {
+                for(int i = depth, j = depth; j < n - depth; j++) {
+                    result.add(matrix[i][j]);
+                }
+            } else {
+                for(int i = depth, j = depth; i < m - depth; i++) {
+                    result.add(matrix[i][j]);
+                }
+            }
+        } 
+        return result;
     }
 }
